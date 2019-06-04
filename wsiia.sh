50ae63eaf3e58521c7fcf714d529e20f8e4ed5b5
Mike Himbeault <mike@forethought.solutions>
2019-06-04 00:14:32 -0500

    Quality of life improvements.

--- wsiia.sh
+++ wsiia.sh
@@ -12,37 +12,40 @@ do
 
     while [ true ]
     do
-        echo "Getting page title for $page_name" >&2
+        echo "$section.$slide Getting page title for $page_name" >&2
         page_title=$(wget -qO- "https://en.wikipedia.org/w/api.php?format=json&action=query&titles=$page_name" | jq -r '(.query.pages | keys)[0] as $k | .query.pages[$k].title')
 
-        echo "Getting images for $page_name" >&2
+        echo "$section.$slide Getting images for $page_name" >&2
         images=$(wget -qO- "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=images&titles=$page_name" | jq -r '(.query.pages | keys)[0] as $k | .query.pages[$k].images | if . == null then null else .[].title end')
 
-        echo "Getting links for $page_name" >&2
+        echo "$section.$slide Getting links for $page_name" >&2
         links=$(wget -qO- "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=links&titles=$page_name" | jq -r '(.query.pages | keys)[0] as $k | .query.pages[$k].links[] | .title')
 
-        echo "Removing TLDs from list of acceptable links" >&2
+        echo "$section.$slide Removing TLDs from list of acceptable links" >&2
         links=$(echo "$links" | grep -v '^\.[a-z-]*')
 
-        echo "Checking image and link counts for $page_name" >&2
-        echo "$images" | wc -l >&2
-        echo "$links" | wc -l >&2
-        if [ "$images" == "null" ] || [ "$links" == "null" ]
+        echo "$section.$slide Limiting images to JPG/JPEG and PNG" >&2
+        images=$(echo "$images" | grep -iE "\.(png|jpg|jpeg)$")
+
+        echo "$section.$slide Checking image and link counts for $page_name" >&2
+        if [ "$images" == "null" ] || [ "$links" == "null" ] || [ "$images" == "" ]
         then
+            echo "$section.$slide No images or links found for the current page" >&2
+            echo "===============" >&2
             echo "$images" | wc -l >&2
             echo "$links" | wc -l >&2
             break
         fi
 
         image_name=$(echo "$images" | shuf | head -n1)
-        echo "Chosen image name: $image_name" >&2
+        echo "$section.$slide Chosen image name: $image_name" >&2
 
-        echo "Testing linked pages to find one that exists" >&2
+        echo "$section.$slide Testing linked pages to find one that exists" >&2
         for page in `echo "$links" | shuf | tr ' ' '_'`
         do
-            echo "Testing $page" >&2
+            echo "$section.$slide Testing $page" >&2
             missing_page=$(wget -qO- "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=links&titles=$page" | jq '.query.pages | has("-1")')
-            
+
             if [ "$missing_page" == "false" ]
             then
                 next_page=$page
@@ -52,11 +55,11 @@ do
 
         if [ "$next_page" == "" ]
         then
-            echo "No linked pages exist, restarting" >&2
+            echo "$section.$slide No linked pages exist, restarting" >&2
             break
         fi
 
-        echo "Chosen next page: $next_page" >&2
+        echo "$section.$slide Chosen next page: $next_page" >&2
 
         image_url=$(wget -qO- "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=imageinfo&iiprop=url&titles=$image_name" | jq -r '(.query.pages | keys)[0] as $k | .query.pages[$k].imageinfo[0].url')
         echo "{\"section\": $section, \"slide\": $slide, \"title\": \"$page_title\", \"image\": \"$image_url\"}"
